b9125884ef387f01d8ad541a2a1aed3c28776988
Rashad Alam <rashadalam005@gmail.com>
2024-09-29 19:07:48 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+
+
+
+const PostDetails = () => {
+
+    const post = useLoaderData();
+    const {id, title, body} = post;
+
+
+    return (
+        <div>
+            <h2>Post Details: {id}</h2>
+            <p>Title: {title}</p>
+            <p><small>{body}</small></p>
+        </div>
+    );
+};
+
+export default PostDetails;
